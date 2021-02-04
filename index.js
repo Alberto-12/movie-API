ec0240948330670cf2d11c481dc71e049d155384
Alberto Serafini <ilgumo@gmai.com>
2021-02-04 12:30:02 -0600

    Added_route_staticPages

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express'),
   morgan = require('morgan');
 
 const app = express();
-
+app.use(express.static('public'));
 app.use(morgan('common'));
 const bodyParser = require('body-parser'),
   methodOverride = require('method-override');
